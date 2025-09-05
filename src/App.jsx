0786caab59df24947cb4212e2cbc6c54cefe5aee
Rangel Diaz <luis.rangeldiaz@gmail.com>
2025-09-05 11:31:30 -0600

    Initial commit Template EUT

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div
+				className='
+  animate-in fade-in
+  slide-in-from-bottom-12
+  zoom-in-95
+  duration-[900ms]
+  ease-[cubic-bezier(.16,1,.3,1)]
+'>
+				Hola — ahora sí debería notarse bastante
+			</div>
+		</>
+	);
+}
+
+export default App;
